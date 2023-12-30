5d5d5272223bcebccc3a3a874878972d195667df
Riad123456789 <riad80717@gmail.com>
2023-12-30 17:21:48 +0600

    exprience section responsive are done

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -2,8 +2,8 @@ import img from "../assets/images/sponsors/architect.png"
 
 const Experience = () => {
     return (
-        <div className="flex mt-14 gap-60  w-[74%] mx-auto relative">
-            <div className=" space-y-3  w-[425px] ">
+        <div className=" flex flex-col lg:flex-row  p-4 pr-1 gap-6 mt-9 md:mt-10  lg:gap-60   lg:w-[74%] mx-auto relative">
+            <div className=" space-y-3  lg:w-[425px] ">
                 <h1 className="text-2xl font-bold border-l-4 border-[#FF900E]  pl-2">Features you will <br />
                     love & enjoy</h1>
                 <p className="text-xs">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even</p>
@@ -27,12 +27,12 @@ const Experience = () => {
 
             <div className="">
                 <div className="">
-                    <img className="w-[440px] h-[620px]" src={img} alt="" />
+                    <img className=" w-[350px] h-[470px]  ml-7 md:h-[50%]  md:w-[90%] md:ml-16  lg:w-[440px] lg:h-[620px]" src={img} alt="" />
                 </div>
 
-                <div className="border w-52 text-center bg-[#FF900E] text-white  py-6 rounded-md absolute 
-                left-[54%] top-[85%]">
-                    <p className="text-4xl font-bold">10+Year</p>
+                <div className="border w-36 md:w-52 text-center bg-[#FF900E] text-white py-4 md:py-6 rounded-md absolute 
+                    left-[2%] top-[94%]  md:left-[1%] md:top-[94%]   lg:left-[58%] lg:top-[83%]">
+                    <p className="text-2xl md:text-4xl font-bold">10+Year</p>
                     <p>Experience</p>
                 </div>
             </div>
